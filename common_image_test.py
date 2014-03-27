0fc0aab023b9feaa44f7c2c5bd0e65a2ac8c85af
Vladimir Elistratov <vvoovv.projects@gmail.com>
2014-03-27 23:39:34 +0400

    initial commit

---
+++ common_image_test.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+modelList = "../models/model_list.txt"
+
+import sys, os
+# append the script's directory to the sys.path
+sys.path.append(os.path.dirname(os.path.realpath(__file__)))
+
+from common_image import CommonImage
+
+# read model file list from modelList
+with open(modelList) as f:
+	files = [line.rstrip() for line in f]
+
+manager = CommonImage(
+	files = files,
+	blenderFilesDir = "../models",
+	gdalDir = "C:/Program Files/GDAL",
+	angleX = -45,
+	angleY = -45,
+	outputImagesDir = "../rasters"
+)
+manager.render()
---
+++ multiple_images_test.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+modelList = "../models/model_list.txt"
+
+import sys, os
+# append the script's directory to the sys.path
+sys.path.append(os.path.dirname(os.path.realpath(__file__)))
+
+from multiple_images import MultipleImages
+
+# read model file list from modelList
+with open(modelList) as f:
+	files = [line.rstrip() for line in f]
+
+manager = MultipleImages(
+	files = files,
+	blenderFilesDir = "../models",
+	outputImagesDir = "C:/Users/vvoovv/Documents/MapBox/project/test-2_5dmaps/models",
+	angleX = -45,
+	angleY = -45,
+	csvFile = "../models/models.csv"
+)
+manager.render()
